be76bc8395e183f3f64e8ae74a0bd521d61fc10f
Snowboundport37 <andrei.gorlitsky@mymail.champlain.edu>
2025-02-18 01:47:25 -0500

    Added secure-ssh.sh script

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# secure-ssh.sh
+# Author: [Your Name]
+# Creates a new ssh user using $1 parameter
+# Adds a public key from the local repo or curled from the remote repo
+# Removes root's ability to ssh in
+
+echo "ALL YOUR CODE GOES HERE"
+
